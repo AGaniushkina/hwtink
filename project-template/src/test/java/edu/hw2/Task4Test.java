81c1d25b6de5d8106bb69d65fc38b90a9aefc16f
AGaniushkina <annaganyushkina02@gmail.com>
2023-11-30 14:20:21 +0400

    hw2

---
+++ project-template/src/test/java/edu/hw2/Task4Test.java
@@ -0,0 +1,14 @@
+package edu.hw2;
+
+import edu.hw2.Task4.Calling;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Task4Test {
+    @Test
+    public void callingInfoTest() {
+        assertEquals("callingInfo", Calling.callingInfo().methodName());
+        assertEquals("edu.hw2.Task4.Calling", Calling.callingInfo().className());
+    }
+}
+
